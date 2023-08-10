529b04241a72d45f37315f111c45861bba3382c0
anikettrana <anikettrana@gmail.com>
2023-08-10 22:23:36 +0530

    creating_navbar

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+
+export default class App extends Component {
+
+  // c="aniket";
+  render() {
+    return (
+      <div>
+<Navbar/>
+        {/* {this.c} */}
+      </div>
+    )
+  }
+}
